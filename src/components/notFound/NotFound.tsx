a9b6fb8cbf91154007733d60e11de0d4233935cf
nam anh tran van <namanh022@gmail.com>
2024-03-08 12:11:14 +0200

    change the notfound styling

--- src/components/notFound/NotFound.tsx
+++ src/components/notFound/NotFound.tsx
@@ -1,17 +1,63 @@
-import { Button, Container, Divider, Paper, Typography } from "@mui/material";
+import {
+  Box,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
 import { Link } from "react-router-dom";
+
 
 const NotFound = () => {
   return (
-    <Container component={Paper} sx={{ height: 400 }}>
-      <Typography gutterBottom variant="h3">
-        Oops - we could not find what you looking for
-      </Typography>
-      <Divider />
-      <Button fullWidth component={Link} to="/">
-        Go back to shop
-      </Button>
-    </Container>
+    <Box marginTop={"8rem"}>
+      <Container maxWidth="md">
+        <Grid container spacing={2} direction={"column"}>
+          <Grid
+            item
+            xs={12}
+            alignItems={"center"}
+            display={"flex"}
+            justifyContent={"center"}
+          >
+            <img
+              src="https://www.breathearomatherapy.com/assets/images/global/no-product.png"
+              alt="img-notFound"
+              style={{ width: "50%", height: "80%", objectFit: "cover" }}
+            />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            display={"flex"}
+            justifyContent={"center"}
+            direction={"column"}
+            alignItems={"center"}
+          >
+            <Typography variant="h3">Sorry</Typography>
+            <Typography variant="h6">
+              The page you’re looking for doesn’t exist.
+            </Typography>
+            <Box
+              sx={{
+                width: { md: "40%", xs: "80%" },
+                backgroundColor: "#f02d34",
+                color: "white",
+                border: "none",
+                marginTop: "20px",
+                borderRadius: "15px",
+                padding: "10px 16px",
+              }}
+              component={Link}
+              to="/"
+              textAlign={"center"}
+              fontWeight={"700"}
+            >
+              Back to Homepage
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
   );
 };
 
